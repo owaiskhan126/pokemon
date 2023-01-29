48bbfbb24efe7bbef36deda81164d57c9fd6c70e
owaiskhan126 <120951137+owaiskhan126@users.noreply.github.com>
2023-01-30 00:07:41 +0500

    new commit

---
+++ calculator/app.js
@@ -0,0 +1,13 @@
+function cal (a){
+    document.getElementById('inp').value +=a
+}
+function sum (){
+    var a = document.getElementById('inp').value
+    document.getElementById('inp').value = eval (a)
+}
+function clr (){
+    document.getElementById ('inp').value = ""
+}
+function clrone (){
+    document.getElementById ('inp')
+}
